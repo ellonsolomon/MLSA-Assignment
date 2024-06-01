8eaef780f08a6f0121a28e488713b323ebe0658d
Ellon Solomon <ellon.solomon@gmail.com>
2024-06-01 22:32:27 +0200

    profile changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Ellon Berhanu",
+  title: "AI / ML Engineer | Researcher ",
+  email: "ellon.solomon@gmail.com",
+  gitHub: "https://github.com/ellonsolomon/",
+  // instagram: "microsoft",
+  linkedIn: "www.linkedin.com/in/ellonsolomon",
+  // medium: "",
+  twitter: "www.twitter.com/ellonsolomon",
+  // youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
